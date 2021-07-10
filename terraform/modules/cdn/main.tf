66dd9310d29f7a56390259e21aca6e763962371b
Pavel Kakolin <appr@appr.me>
2021-07-10 17:30:11 +0200

    cloudfront: use latest security policy for viewer connections

--- terraform/modules/cdn/main.tf
+++ terraform/modules/cdn/main.tf
@@ -85,7 +85,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = false
     acm_certificate_arn            = var.certificate_arn
-    minimum_protocol_version       = "TLSv1.2_2019"
+    minimum_protocol_version       = "TLSv1.2_2021"
     ssl_support_method             = "sni-only"
   }
 
